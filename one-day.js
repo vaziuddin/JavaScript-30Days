d52d868b91a04c58e8d4166d5462397b07b67f17
vaziuddin <vaziuddin.jits@gmail.com>
2021-02-23 21:46:27 +0530

    One day

---
+++ one-day.js
@@ -0,0 +1,62 @@
+//Motivation
+//A Computer
+//Internet
+//A Browser
+//A Code Editor
+
+console.log('Hello', 'World','!')//'Hello' 'World' '!'
+console.log('Happy','NEW','YEAR',2021)//
+console.log('Welcome','to', 30,'Days','Of','JavaScript')//'Welcome' 'to' 30 'Days' 'Of' 'JavaScript'
+
+//Comments
+console.log('Hello, World!')//'Hello, World!'
+console.log('Hello, World!')//'Hello, World!'
+console.log(`Hello, Wolrd!`)//'Hello, Wolrd!'
+console.log(2+3)//5
+console.log(3-2)//1
+console.log(2*3)//6
+console.log(3/2)//1.5
+console.log(3%2)//1
+console.log(3**2)//9
+
+console.log('Welcome to 30DaysOfJavaScript')//'Welcome to 30DaysOfJavaScript'
+
+console.log(typeof 'Vaziuddin')//'string'
+console.log(typeof 5)//'number'
+console.log(typeof true)//'boolean'
+console.log(typeof null)//'object'
+console.log(typeof undefined)//'undefined'
+
+//Single line Commenting
+//Multiline Commenting
+
+//commenting the code itself with a single comment
+//let firstname = 'vaziuddin' single line comment
+//let lastname = 'mohammed' single line comment
+
+//MultiLine Commenting
+/* let location = 'Hyderabad';
+let age = 100;
+let isMarried = true;
+This is a Multiple line comment*/
+
+let firstname = 'Vaziuddin'
+let lastname = 'Mohammed'
+let country = 'India'
+let city = 'Hyderabad'
+let age = 100
+let isMarried = true
+
+console.log(firstname, lastname, country, city, age, isMarried)//'Vaziuddin' 'Mohammed' 'India' 'Hyderabad' 100 true
+
+//let age =30
+const gravity = 9.81
+const boilingPoint =100
+const PI = 3.14
+
+console.log(age, gravity, boilingPoint, PI)//100 9.81 100 3.14
+
+let name = 'Vaziuddin',
+job = 'teacher',
+live ='India'
+console.log(name, job, live)//'Vaziuddin' 'teacher' 'India'
