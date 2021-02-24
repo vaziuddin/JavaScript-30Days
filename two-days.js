9e76bea8d5f6e89e3230a1964730ac549b18aec1
vaziuddin <vaziuddin.jits@gmail.com>
2021-02-24 12:47:44 +0530

    add file Two-days

---
+++ two-days.js
@@ -0,0 +1,68 @@
+let word = 'JavaScript'
+word[0] ='Y'
+
+let numOne =3 
+let numTwo = 3
+console.log(numOne==numTwo)
+
+let js = 'JavaScript'
+let py = 'Python'
+
+console.log(js==py)
+
+let lightOn = true 
+let lightOff = false
+console .log(lightOn == lightOff)
+
+let nums  = [1, 2, 3]
+nums[0] =10
+
+console.log(nums)
+let nums1 = [1, 2, 3]
+let numbers = [1, 2, 3]
+console.log(nums1 == numbers)
+console.log(nums1=== numbers)
+
+let userOne = {
+  name:'Vaziuddin',
+  role:'teaching',
+  country:'India'
+}
+let userTwo = {
+  name : 'Vaziuddin',
+  role : 'teaching',
+  country : 'India'
+}
+console.log(userOne==userTwo)
+console.log(userOne ===userTwo)
+
+let nums2  = [1, 2, 3]
+let numbers2 = nums2
+console.log(nums2==numbers2)
+console.log(nums2 === numbers2)
+
+//NUMBERS
+let age = 35
+const gravity = 9.81
+let mass = 72
+const PI = 3.14
+
+const biolingPoint = 100
+const bodyTemp = 37
+console.log(age, gravity, mass, PI, biolingPoint, bodyTemp)
+
+//Math Object
+const Pi = Math.PI
+console.log(Pi)
+
+console.log(Math.round(Pi))
+console.log(Math.round(9.81))
+console.log(Math.floor(Pi))
+console.log(Math.ceil(Pi))
+console.log(Math.min(-5, 3, 20, 4, 5, 10))
+console.log(Math.max(-5, 3, 20, 4, 5, 10))
+const randNum = Math.random()
+console.log(randNum)
+const num = Math.floor(Math.random()*11)
+console.log(num)
+
