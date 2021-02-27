4c593e2d82f90211f39545087f765238304854a6
vaziuddin <vaziuddin.jits@gmail.com>
2021-02-27 18:49:37 +0530

    add three days exercise

--- two-days.js
+++ two-days.js
@@ -137,3 +137,120 @@ console.log(personInfoThree)
 let c = 2
 let d = 3
 console.log(`${c} is greater than ${d}:${c>d}`)
+//STRING METHODS
+//let js1 = 'JavaScript'
+console.log(js.length)
+console.log(firstName.length)
+let firstLetter = js[0]
+console.log(firstLetter)
+let secondLetter = js[1]
+let thirdLetter = js[2]
+let lastLetter = js[9]
+console.log(lastLetter)
+let lastIndex = js.length-1
+console.log(lastIndex)
+console.log(js[lastIndex])
+
+//toUpperCase()
+console.log(js.toUpperCase())
+console.log(firstName.toUpperCase())
+console.log(country.toUpperCase())
+console.log(city.toUpperCase())
+//toLowerCase()
+console.log(js.toLowerCase())
+console.log(firstName.toLowerCase())
+console.log(country.toLowerCase())
+console.log(city.toLowerCase())
+//substr
+console.log(js.substr(4, 6))
+console.log(country.substr(3, 4))
+//substring
+console.log(js.substring(0, 4))
+console.log(js.substring(4, 10))
+console.log(js.substring(4))
+console.log(country.substring(0, 3))
+console.log(country.substring(3,5))
+console.log(country.substring(3))
+//Split
+let string = '30 Days Of JavaScript'
+console.log(string.split())
+console.log(string.split(' '))
+console.log(firstName.split())
+console.log(firstName.split(''))
+let countries = 'Finland, Sweden, Norway, Denmark, and Iceland'
+console.log(countries.split(','))
+console.log(countries.split(', '))
+//Trim
+let str1 = '  30 Days Of JavaScript  '
+console.log(str1)
+console.log(str1.trim(' '))
+let firstname = ' Asabeneh '
+console.log(firstname)
+console.log(firstname.trim())
+//Includes
+console.log(str1.includes('Days'))
+console.log(str1.includes('days'))
+console.log(str1.includes('Script'))
+console.log(str1.includes('script'))
+console.log(str1.includes('java'))
+console.log(str1.includes('Java'))
+
+
+console.log(country.includes('in'))
+console.log(country.includes('In'))
+console.log(country.includes('ind'))
+console.log(country.includes('Ind'))
+
+//Replace
+console.log(string.replace('JavaScript', 'Python'))
+console.log(country.replace('In', 'An In'))
+//CharAt
+console.log(js.charAt(0))
+console.log(js.charAt(lastIndex))
+//charCodeAt
+console.log(js.charCodeAt(3))
+console.log(js.charCodeAt(lastIndex))
+//indexOf
+console.log(str1.indexOf('3'))
+console.log(str1.indexOf('Days'))
+console.log(str1.indexOf('days'))
+console.log(str1.indexOf('a'))
+console.log(str1.indexOf('JavaScript'))
+console.log(str1.indexOf('Script'))
+console.log(str1.indexOf('script'))
+//lastIndexOf
+let strings = 'I love JavaScript. If you do not love JavaScript what else can you love'
+console.log(strings.lastIndexOf('love'))
+console.log(strings.lastIndexOf('you'))
+console.log(strings.lastIndexOf('JavaScript'))
+//concat
+let day = '30'
+console.log(day.concat("Days", "Of","JavaScript"))
+
+console.log(country.concat("n"))
+//startsWith
+let str2 = 'Love is the best to in this World'
+console.log(str2.startsWith('Love'))
+console.log(str2.startsWith('love'))
+console.log(str2.startsWith('world'))
+console.log(country.startsWith('In'))
+console.log(country.startsWith('in'))
+console.log(country.startsWith('an'))
+//endsWith
+console.log(str2.endsWith('World'))
+console.log(str2.endsWith('love'))
+console.log(str2.endsWith('in this World'))
+console.log(country.endsWith('ia'))
+console.log(country.endsWith('in'))
+console.log(country.endsWith('In'))
+//search
+console.log(strings.search('love'))
+console.log(strings.search(/javascript/gi))
+//Match
+let love = 'love'
+let patternOne = /love/
+let patternTwo = /love/gi
+console.log(strings.match('love'))
+console.log(strings.match(patternTwo))
+//repeat
+console.log(love.repeat(10))
