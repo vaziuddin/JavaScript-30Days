7dc7cf29a559f8dd0c6a13694c077259d6f5bd15
vaziuddin <vaziuddin.jits@gmail.com>
2021-03-25 23:54:06 +0530

    Add files via upload

---
+++ six-days.js
@@ -0,0 +1,54 @@
+//Days-6
+//Loops
+// for(let i=0;i<=5;i++){
+//   console.log(i)
+// }
+// for(let i=5;i>=0;i--){
+//   console.log(i)
+// }
+// for(let i=0;i<=5;i++){
+//   console.log(`${i}*${i}=${i*i}`)
+// }
+
+// const countries = ['Finland', 'Sweden', 'Denmark', 'Norway','Iceland']
+// const newArr = []
+// for(let i=0;i<countries.length;i++){
+//   newArr.push(countries[i].toUpperCase())
+// }
+// console.log(newArr)
+
+// const numbers = [1, 2, 3, 4, 5]
+// let sum =0
+// for(let i =0;i<numbers.length;i++){
+//   sum = sum+numbers[i]
+// }
+// console.log(sum)
+// const sqrArr=[]
+// for(let j=0;j<numbers.length;j++){
+//   sqrArr.push(j*j)
+// }
+// console.log(sqrArr)
+
+// const countArr = []
+// for(let i=0;i<countries.length;i++){
+//   countArr.push(countries[i].toUpperCase())
+// }
+// console.log(countArr)
+//while loop
+// let k=0
+// while(k<5){
+//   console.log(k)
+//   k++
+// }
+
+// let i=0
+// do {
+//   console.log(i)
+//   i++
+// } while(i<5)
+
+  //For of loop
+const numbers = [1, 2, 3, 4, 5]
+for( const num of numbers){
+  console.log(num)
+}
